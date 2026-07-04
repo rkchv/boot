da659d282011a4e0c9ddb9ffb07fbfcfaf52cafb
Roman Kochnev <hello@kochnev.tech>
2026-07-04 23:12:37 +0300

    some legacy tweaks

--- run.local.sh
+++ run.local.sh
@@ -33,23 +33,12 @@ brew install tmuxinator
 brew install coreutils
 brew install cmake
 brew install ninja
+brew install gettext
 brew install --cask raycast
 
 asdf plugin add golang https://github.com/kennyp/asdf-golang.git
 asdf install golang latest
 asdf set --home latest
-
-asdf plugin add golangci-lint https://github.com/hypnoglow/asdf-golangci-lint.git
-asdf install golangci-lint latest
-asdf set --home golangci-lint latest
-
-asdf plugin add go-swagger https://github.com/jfreeland/asdf-go-swagger.git
-asdf install go-swagger latest
-asdf set --home go-swagger latest
-
-asdf plugin add mockery https://github.com/tolbier/asdf-mockery.git
-asdf install mockery latest
-asdf set --home mockery latest
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs latest
@@ -59,4 +48,4 @@ asdf plugin add lua https://github.com/Stratus3D/asdf-lua.git
 asdf install lua 5.1
 asdf set --home lua 5.1
 
-ansible-playbook ./local.yml --ask-become-pass --vault-password-file ./secret.txt
+ansible-playbook ./local.yml --ask-become-pass --ask-vault-pass
